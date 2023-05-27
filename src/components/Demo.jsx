660cd21f659b241b39a6ecedd84eb47b5cc22e04
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-05-27 21:10:14 +0200

    handling copying and removing articles

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,47 +1,75 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-import { copy, linkIcon, loader, tick } from '../assets'
-import { useLazyGetSummaryQuery } from '../services/article'
+import {
+  copy,
+  linkIcon,
+  loader,
+  tick,
+  chevron,
+  chevronUp,
+  remove,
+} from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url: '',
-    summary: ""
-  })
-  const [allArticles, setAllArticles] = useState([])
-  const [copied, setCopied] = useState("")
+    url: "",
+    summary: "",
+  });
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
+  const [openSummary, setOpenSummary] = useState(null);
 
-  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
-    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
 
-    if(articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage)
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
     }
-  }, [])
+  }, []);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const { data } = await getSummary({ articleUrl: article.url })
+    const { data } = await getSummary({ articleUrl: article.url });
 
-    if(data?.summary) {
-      const newArticle = { ...article, summary: data.summary }
-      const updatedAllArticles = [newArticle, ...allArticles]
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
 
-      setArticle(newArticle)
-      setAllArticles(updatedAllArticles)
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
 
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-  }
+  };
 
   const handleCopy = (copyUrl) => {
-    setCopied(copyUrl)
-    navigator.clipboard.writeText(copyUrl)
-    setTimeout(() => setCopied(false), 3000)
-  }
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(null), 2000);
+  };
+
+  const handleOpen = (chosen) => {
+    if (chosen === openSummary) {
+      setOpenSummary(null);
+    } else {
+      setOpenSummary(chosen);
+    }
+  };
+
+  const handleRemove = (removeUrl) => {
+    const afterRemove = allArticles.filter(
+      (article) => article.url !== removeUrl
+    );
+    setAllArticles(afterRemove);
+    localStorage.removeItem("articles");
+    localStorage.setItem("articles", JSON.stringify(afterRemove));
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -50,17 +78,18 @@ const Demo = () => {
           className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
         >
-          <img 
-            src={linkIcon} 
-            alt="link_icon" 
-            className="absolute left-0 my-2 ml-3 w-5" 
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
           />
 
           <input
             type="url"
+            name="url"
             placeholder="Enter a URL"
             value={article.url}
-            onChange={(e) => setArticle({...article, url: e.target.value})}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
@@ -75,21 +104,61 @@ const Demo = () => {
 
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           {allArticles.map((item, index) => (
-            <div 
-              key={`link-${index}`}
-              onClick={() => {}}
-              className="link_card"
-            >
-              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
-                <img 
-                  src={copied === item.url ? tick : copy} 
-                  alt="copy_icon"
-                  className="w-[40%] h-[40%] object-contain"
-                />
+            <div key={`article-${index}`}>
+              <div
+                onClick={() => handleOpen(item.url)}
+                className="link_card cursor-pointer"
+              >
+                <div className="copy_btn " onClick={() => handleCopy(item.url)}>
+                  <img
+                    src={copied === item.url ? tick : copy}
+                    alt="copy_icon"
+                    className="w-[50%] h-[50%] object-contain"
+                  />
+                </div>
+                <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                  {item.url}
+                </p>
+                <div className="copy_btn" onClick={() => handleOpen(item.url)}>
+                  <img
+                    src={openSummary === item.url ? chevronUp : chevron}
+                    alt="show_more_icon"
+                    className="w-[60%] h-[60%] object-contain"
+                  />
+                </div>
+                <div
+                  className="copy_btn"
+                  onClick={() => handleRemove(item.url)}
+                >
+                  <img
+                    src={remove}
+                    alt="show_more_icon"
+                    className="w-[60%] h-[60%] object-contain"
+                  />
+                </div>
               </div>
-              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
-                {item.url}
-              </p>
+              {openSummary === item.url && (
+                <div className="flex flex-col gap-3 mt-5 ">
+                  <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                    Article <span className="blue_gradient">Summary</span>
+                  </h2>
+                  <div
+                    className="summary_box cursor-pointer"
+                    onClick={() => handleCopy(item.summary)}
+                  >
+                    <div className="copy_btn">
+                      <img
+                        src={copied === item.summary ? tick : copy}
+                        alt="copy_icon"
+                        className="w-[60%] h-[60%] object-contain"
+                      />
+                    </div>
+                    <p className="font-inter font-medium text-sm text-gray-700 mt-3">
+                      {item.summary}
+                    </p>
+                  </div>
+                </div>
+              )}
             </div>
           ))}
         </div>
@@ -113,14 +182,16 @@ const Demo = () => {
                 Article <span className="blue_gradient">Summary</span>
               </h2>
               <div className="summary_box">
-                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
               </div>
             </div>
           )
         )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
